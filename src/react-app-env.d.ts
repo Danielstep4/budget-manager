eca088833b795fe023072c9d98aa36395397195a
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-22 15:27:30 +0300

    Theming done.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="react-scripts" />
-import { createTheme, ThemeOptions } from "@material-ui/core/styles";
-// #TODO: Add if necessary
-declare module "@material-ui/core/styles" {
-  interface ThemeOptions {}
-}
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,6 +1,27 @@
 import { createTheme } from "@material-ui/core/styles";
 
 // #TODO: Create custom theme
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: "#7C5DF9",
+      light: "#bdadfc",
+      dark: "#2f07cd",
+    },
+    secondary: {
+      main: "#141625",
+      dark: "#0a0a12",
+      light: "#6b73af",
+    },
+    background: {
+      paper: "#1F213A",
+      default: "#141625",
+    },
+  },
+});
 
 export default theme;
