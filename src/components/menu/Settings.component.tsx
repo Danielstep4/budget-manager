01e65fafd2869969e1aff4194c5cfb049b648869
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-24 16:25:45 +0300

    changed signout function

--- src/components/menu/Settings.component.tsx
+++ src/components/menu/Settings.component.tsx
@@ -1,14 +1,19 @@
-import { Box, Typography, useTheme } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import { useAuth } from "../../context/AuthContext";
+import { useBackdrop } from "../../context/BackdropContext";
 import Button from "../global/Button.component";
 
 const Settings: React.FC = () => {
-  const theme = useTheme();
   const { signOut } = useAuth();
+  const { setBackdropOpen } = useBackdrop();
+  const handleLogoutClick = () => {
+    setBackdropOpen(false);
+    signOut();
+  };
   return (
     <Box display="flex" justifyContent="space-between">
       <Typography variant="h4">Settings</Typography>
-      <Button onClick={() => signOut()}>Logout</Button>
+      <Button onClick={handleLogoutClick}>Logout</Button>
     </Box>
   );
 };
