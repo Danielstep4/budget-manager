d867584e2f2e5c3e852e07f0f82e80f5fa1e0d89
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-31 16:56:40 +0300

    Added real data to chart

--- src/components/content/Charts/Pie.component.tsx
+++ src/components/content/Charts/Pie.component.tsx
@@ -1,28 +1,35 @@
 import { Box, Typography, useTheme } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ResponsiveContainer, Pie, PieChart, Cell } from "recharts";
+import { useAuth } from "../../../context/AuthContext";
 import { getTotalFlow } from "../../../utils/db/flow";
 import { getSymbol } from "../../../utils/getSymbol";
 
 const PieComp: React.FC = () => {
   const theme = useTheme();
-  const DATA = getTotalFlow();
+  const { currentUser } = useAuth();
+  const [data, setData] = useState<any>([]);
+  useEffect(() => {
+    getTotalFlow(currentUser!.uid)
+      .then((result) => setData(result))
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <Box position="fixed" right="0" top="0" height="100%">
       <ResponsiveContainer width={700} height="80%">
         <PieChart width={730} height={250}>
           <Pie
-            data={DATA}
-            dataKey={(DATA) => DATA.value}
-            nameKey={(DATA) => DATA.name}
+            data={data}
+            dataKey={(data) => data.value}
+            nameKey={(data) => data.name}
             innerRadius={100}
             outerRadius={120}
             stroke={theme.palette.common.black}
             paddingAngle={5}
-            label={(DATA) => DATA.name + " - " + DATA.value + getSymbol("ILS")}
+            label={(data) => data.name + " - " + data.value + getSymbol("ILS")}
             labelLine={false}
           >
-            {DATA.map((obj, i) => (
+            {data.map((obj: any, i: number) => (
               <Cell
                 key={`cell-${i}`}
                 fill={
--- src/utils/db/flow.ts
+++ src/utils/db/flow.ts
@@ -70,7 +70,22 @@ export const getFlow = async (uid: string) => {
   }
 };
 // In this branch its demo data, will fix it in caching branch.
-export const getTotalFlow = () => {
+export const getTotalFlow = async (uid: string) => {
+  const flow = await getFlow(uid);
+  if (flow) {
+    const { expenses, incomes } = flow;
+    const totalExpenses = expenses.reduce((acc, curr) => acc + curr.amount, 0);
+    const totalIncomes = incomes.reduce((acc, curr) => acc + curr.amount, 0);
+
+    return [
+      {
+        name: "Expenses",
+        value: totalExpenses,
+      },
+      { name: "Incomes", value: totalIncomes },
+    ];
+  }
+
   return [
     {
       name: "Expenses",
